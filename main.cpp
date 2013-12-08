c5b6cf1323e69896aa9754376ddbf4cee1d87f1d
AlexMartinez <alexandrecetto@gmail.com>
2013-12-08 11:18:38 +0100

    Création du git avec fichiers de base

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  projet_algo
+//
+//  Created by Alex on 08/12/2013.
+//  Copyright (c) 2013 Alex&Olivier. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
